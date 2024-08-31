a58ff4e61c4fcc87f6e7f942be7d130f305bb68a
Alejandro Ordaz <oordaz@nrtec.com.mx>
2024-08-31 10:47:04 -0600

    Estilos en la vista de login

--- lib/login.dart
+++ lib/login.dart
@@ -23,8 +23,19 @@ class _LoginState extends State<Login> {
           Padding(
             padding: EdgeInsets.all(50.0),
             child: Container(
-              color: Color(0xFFb9a8c), ///Valor hexadecimal del color
+              height: 800, ///Agrega un tamaño al contenedor
+              ///Se puede agregar padding como atributo del contenedor
+              padding: EdgeInsets.all(20.0),
+              color: Color(0xFFb9a8c), ///Valor hexadecimal del color  -------->
+              ///quita las esquina rectangulares por bordes redondeados          Al tener estos dos atributos
+              decoration: BoxDecoration(      ///                                juntos, marcará un error por
+                borderRadius: BorderRadius.circular(40),    ///                  incompatibilidad de atributos
+                color: Colors.black54, /// ------------------------------------>
+              ),
               child: Column(
+                ///Atributo de Column para indicar el tamaño que debe tener por
+                ///el espacio que ocupan sus hijos widgets
+                mainAxisSize: MainAxisSize.min,
                 children: [
                   const Text(
                     'LOGIN',
@@ -38,14 +49,59 @@ class _LoginState extends State<Login> {
                   const Text('Usuario/correo/telefono:'),
                   TextFormField(
                     controller: user,
+                    ///Crear un recuadro más estilizado
+                    decoration: InputDecoration(
+                      ///Agregar un recuadro con bordes redondeado
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(
+                          width: 1,
+                          style: BorderStyle.none,
+                        ),
+                      ),
+                      ///Agregar color interno del input
+                      filled: true,
+                      fillColor: Colors.white,
+                      ///Agregar un icono dentro del input
+                      prefixIcon: const Icon(Icons.person),
+                        ///Agregar texto de ayuda dentro del input
+                      hintText: 'Arega tu usuario'
+                    ),
                   ),
                   const Text('Contraseña:'),
                   TextFormField(
                     controller: pass,
                     obscureText: true, ///Ocultar el texto normal
+                    ///Crear un recuadro más estilizado
+                    decoration: InputDecoration(
+                      ///Agregar un recuadro con bordes redondeado
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: const BorderSide(
+                            width: 1,
+                            style: BorderStyle.none,
+                          ),
+                        ),
+                        ///Agregar color interno del input
+                        filled: true,
+                        fillColor: Colors.white,
+                        ///Agregar un icono dentro del input
+                        prefixIcon: const Icon(Icons.person),
+                        ///Agregar texto de ayuda dentro del input
+                        hintText: 'Arega tu usuario'
+                    ),
                   ),
                   const SizedBox(height: 20.0,),
                   ElevatedButton(
+                    ///Atributo para agregar estilo al botón
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue, ///Color al botón
+                      ///Agrega borde redondeado en las esquinas
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      fixedSize: Size(200,40), ///Tamaño que tendrá el botón
+                    ),
                     onPressed: () {
                       ///Detectar los cambios de estado de nuestra vista actual
                       setState(() {
