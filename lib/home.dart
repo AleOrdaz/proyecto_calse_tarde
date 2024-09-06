42edb25ee45e72d4bc5943a92424d7e1e0ad93b7
Alejandro Ordaz <oordaz@nrtec.com.mx>
2024-09-05 20:04:59 -0600

    Tarea

--- lib/home.dart
+++ lib/home.dart
@@ -11,14 +11,56 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
+    ///Obtener el tamaño de la pantalla actual
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       body: Stack(
         children: [
           SingleChildScrollView(
             child: Column(
               children: [
+                ///for(int i = 0; i < 5; i++)
+                /// createdCard(texto: "Hola 1")
+                InkWell(
+                  onTap: () {
+                    setState(() {
+                      bandCambiaDeColor = bandCambiaDeColor ? false : true;
+                    });
+                  },
+                  child: bandCambiaDeColor ? Container(
+                    color: bandCambiaDeColor ? Colors.red : Colors.green,
+                    height: 50,
+                    width: 50,
+                  ) : Container(),
+                ),
 
-                createdCard(texto: "Hola 1"),
+                Container(
+                  ///MediaQuery.of(context).size.height / 2 ó * 0.5 -> 50%
+                  height: size.height, ///100%
+                  width: size.width, ///100%
+                  child: ///Nos permite manejar listas de datos para construir widgtes
+                  ListView.builder(
+                      padding: const EdgeInsets.all(8),
+                      itemCount: con.lista.length, ///n que itera o construye sus widgets
+                      itemBuilder: (BuildContext context, int index) {
+                        ///Area de construcción NO PASAR
+
+                        String varianle = '';
+                        ///Tipo dinamico o que se adapta al valor que se asigna
+                        ///datos = [1, Text1, textN1, 101]
+                        var datos = con.lista[index].toString().split('#'); ///$ -> NO USAR
+
+                        return createdCard(
+                          textoN: datos[1],
+                          texto: datos[2],
+                          numero: datos[3],
+                          id: datos[0],
+                        );
+                      }
+                    //const SizedBox(height: 35.0,),
+                  ),
+                )
+
               ],
             ),
           )
@@ -31,9 +73,13 @@ class _HomeState extends State<Home> {
 
 ///Clase para crear widgets  o creador de widgets sin estado
 class createdCard extends StatelessWidget {
+  final String textoN;
   final String texto;
+  final String numero;
+  final String id;
   const createdCard({
-    super.key, required this.texto,
+    super.key, required this.texto, required this.textoN,
+    required this.numero, required this.id,
   });
 
   @override
@@ -51,7 +97,15 @@ class createdCard extends StatelessWidget {
             child: Icon(Icons.edit),),
           Expanded(
               flex: 1, ///1.2 = 10%
-              child: Icon(Icons.delete))
+              ///Le da propieddad de realizar la función de botón
+              child: InkWell(
+                onTap: () {
+                  print("fue presionado el botón de eliminar id: $id");
+                },
+                child: Icon(Icons.delete),
+              )
+
+          )
         ],
       ),
     );
